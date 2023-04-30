7248f5bebe2be7b17f8013cd4611c2aa3eb91085
Ravshanbekk07 <ramazonof07@gmail.com>
2023-04-30 13:30:39 +0500

    solvved

--- basic_func10.py
+++ basic_func10.py
@@ -3,3 +3,17 @@
 # Create function arguments a.
 
 # Return the opposite value of a.
+
+def main(a):
+    
+    return a/(-1)
+
+o = main(5)
+print(o)
+
+def main(a):
+    
+    return a/(-1)
+
+o = main(1)
+print(o)
