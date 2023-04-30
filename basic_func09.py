6cfa03a96f05f8a1b821ac113888d3f85e0a843a
Ravshanbekk07 <ramazonof07@gmail.com>
2023-04-30 12:13:44 +0500

    solvved

--- basic_func09.py
+++ basic_func09.py
@@ -3,3 +3,16 @@
 # Create function arguments a.
 
 # decrease the value of a to one and return
+def main(a):
+    
+    return a-1
+
+o = main(5)
+print(o)
+
+def main(a):
+    
+    return a-1
+
+o = main(8)
+print(o)
